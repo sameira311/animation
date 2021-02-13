6b6c43818e3f9180b90ce9c22c01a8f4b6ebe686
sameira311 <78248205+sameira311@users.noreply.github.com>
2021-02-13 19:42:37 +0530

    3D animation

---
+++ Mario_animation/Assets/scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private Animator _animator;
+    [SerializeField] private Rigidbody2D _rigidbody2d;
+    [SerializeField] private float _speed = 2f;
+
+    private Vector2 _movement;
+
+    // Update is called once per frame
+    void Update()
+    {
+        _movement.x = Input.GetAxis("Horizontal");
+        _movement.y = Input.GetAxis("Vertical");
+
+
+        _animator.SetFloat("Horizontal", _movement.x);
+        _animator.SetFloat("Vertical", _movement.y);
+        _animator.SetFloat("Speed", _movement.sqrMagnitude);
+    }
+
+    private void FixedUpdate() {
+        _rigidbody2d.velocity = new Vector2(_movement.x * _speed * Time.fixedDeltaTime, _movement.y * _speed * Time.deltaTime);
+    }
+}
